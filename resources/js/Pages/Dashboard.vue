8041cfb7f2425091c6abbcf48f8fafa3645c3a88
Juan Carlos Justiniano Coelho <jcjustinianoc@hotmail.com>
2025-05-13 19:36:36 -0500

    Mudança dashboard

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -16,7 +16,9 @@ import { Head } from "@inertiajs/vue3";
         <div class="py-12">
             <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
                 <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900">You're logged in!</div>
+                    <div class="p-6 text-gray-900">
+                        Futuro tela de dashboard!!!
+                    </div>
                 </div>
             </div>
         </div>
